ba6f13a66b742f2d0c3e57987b2b7109401916c3
uhziel <uhziel@gmail.com>
2024-11-15 12:00:04 +0800

    允许 obsidian.nvim 修改 tags

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -23,9 +23,26 @@ return {
       daily_notes = {
         folder = 'diary-2024',
       },
-      disable_frontmatter = true,
       note_id_func = function(title)
         return title
+      end,
+      note_frontmatter_func = function(note)
+        -- Add the title of the note as an alias.
+        if note.title then
+          note:add_alias(note.title)
+        end
+
+        local out = { aliases = note.aliases, tags = note.tags }
+
+        -- `note.metadata` contains any manually added fields in the frontmatter.
+        -- So here we just make sure those fields are kept in the frontmatter.
+        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+          for k, v in pairs(note.metadata) do
+            out[k] = v
+          end
+        end
+
+        return out
       end,
     },
   },
